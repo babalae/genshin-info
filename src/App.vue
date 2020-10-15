81d08210b82c583d1069a87835da7d8334b2b279
huiyadanli <huiyadanli@gmail.com>
2020-10-15 22:02:14 +0800

    R.I.P

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,9 @@
     </nav>
     <section class="section">
       <div class="container">
+        <article class="message is-warning">
+          <div class="message-body">NGA有mhy内鬼，当前米游社角色页面已经打不开了，本项目完结...</div>
+        </article>
         <div class="field has-addons">
           <p class="control">
             <input id="uid" class="input" type="text" placeholder="请输入UID" v-model="uid" @keyup.enter="search">
