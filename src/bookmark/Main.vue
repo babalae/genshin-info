6e45cd150f5364a96affab997618c3540fedb4a9
huiyadanli <huiyadanli@gmail.com>
2021-08-14 23:55:08 +0800

    修复查询状态直接跳转成功的问题

---
+++ src/bookmark/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="">
+          原神角色信息查询
+        </a>
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+    <section class="section" ref="displayPanel">
+      <div class="container">
+        <div v-if="baseInfo !== undefined">
+          <BaseInfo :base-info-data="baseInfo"></BaseInfo>
+        </div>
+        <div v-if="abyssInfo !== undefined">
+          <AbyssInfo :abyss-info-data="abyssInfo"></AbyssInfo>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import BaseInfo from './../components/BaseInfo.vue'
+import AbyssInfo from './../components/AbyssInfo.vue'
+export default {
+  name: "genshin-info-webpack2",
+  components: {BaseInfo, AbyssInfo},
+  props: ['baseInfo', 'abyssInfo']
+}
+</script>
+
+<style scoped>
+
+</style>
