a2eca33ecc5bb22e26a93432aee1723229556197
huiyadanli <huiyadanli@gmail.com>
2021-11-03 21:31:01 +0800

    添加雷神瞳、奇馈宝箱数的显示

--- src/components/base/SummaryBox.vue
+++ src/components/base/SummaryBox.vue
@@ -15,20 +15,6 @@
       </div>
       <div class="column is-one-quarter">
         <div>
-          <p class="heading heading-custom">风神瞳</p>
-          <p class="title title-custom">{{ statsData.anemoculus_number }}</p>
-        </div>
-      </div>
-      <div class="column is-one-quarter">
-        <div>
-          <p class="heading heading-custom">岩神瞳</p>
-          <p class="title title-custom">{{ statsData.geoculus_number }}</p>
-        </div>
-      </div>
-    </div>
-    <div class="columns is-multiline is-mobile has-text-centered">
-      <div class="column is-one-quarter">
-        <div>
           <p class="heading heading-custom">获得角色数</p>
           <p class="title title-custom">{{ statsData.avatar_number }}</p>
         </div>
@@ -39,20 +25,40 @@
           <p class="title title-custom">{{ statsData.way_point_number }}</p>
         </div>
       </div>
+    </div>
+    <div class="columns is-multiline is-mobile has-text-centered">
+      <div class="column is-one-quarter">
+        <div>
+          <p class="heading heading-custom">风神瞳</p>
+          <p class="title title-custom">{{ statsData.anemoculus_number }}</p>
+        </div>
+      </div>
+      <div class="column is-one-quarter">
+        <div>
+          <p class="heading heading-custom">岩神瞳</p>
+          <p class="title title-custom">{{ statsData.geoculus_number }}</p>
+        </div>
+      </div>
+      <div class="column is-one-quarter">
+        <div>
+          <p class="heading heading-custom">雷神瞳</p>
+          <p class="title title-custom">{{ statsData.electroculus_number }}</p>
+        </div>
+      </div>
       <div class="column is-one-quarter">
         <div>
           <p class="heading heading-custom">解锁秘境</p>
           <p class="title title-custom">{{ statsData.domain_number }}</p>
         </div>
       </div>
+    </div>
+    <div class="columns is-multiline is-mobile has-text-centered">
       <div class="column is-one-quarter">
         <div>
           <p class="heading heading-custom">深境螺旋</p>
           <p class="title title-custom">{{ statsData.spiral_abyss }}</p>
         </div>
       </div>
-    </div>
-    <div class="columns is-multiline is-mobile has-text-centered">
       <div class="column is-one-quarter">
         <div>
           <p class="heading heading-custom">华丽宝箱数</p>
@@ -71,10 +77,18 @@
           <p class="title title-custom">{{ statsData.exquisite_chest_number }}</p>
         </div>
       </div>
+    </div>
+    <div class="columns is-multiline is-mobile has-text-centered">
       <div class="column is-one-quarter">
         <div>
           <p class="heading heading-custom">普通宝箱数</p>
           <p class="title title-custom">{{ statsData.common_chest_number }}</p>
+        </div>
+      </div>
+      <div class="column is-one-quarter">
+        <div>
+          <p class="heading heading-custom">奇馈宝箱数</p>
+          <p class="title title-custom">{{ statsData.magic_chest_number }}</p>
         </div>
       </div>
     </div>
